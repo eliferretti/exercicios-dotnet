33ee732c24b0d92d82d0a7024a52da7d178282df
Eli Silva Ferretti Filho <eliferretti14@gmail.com>
2024-08-15 12:29:27 -0300

    tipos e operadores

---
+++ 1 - Introducao ao C#/1 - variaveis-operadores/Exercicios.cs
@@ -0,0 +1,52 @@
+﻿namespace tipos_operadores_exercicios;
+
+public class Exercicios
+{
+/* 
+    Exercício 1
+    Crie um método ConcatenaNome para concatenar seu nome completo.
+    Crie duas váriáveis do tipo texto, armazene respectivamente seu primeiro nome e seu ultimo nome, 
+    exiba na tela seu nome completo concatenando as variáveis.
+*/
+    public void ConcatenaNome()
+    {
+        string primeiroNome = "Eli";
+        string ultimoNome = "Filho";
+
+        Console.WriteLine($"Meu nome completo é: {primeiroNome} {ultimoNome}");
+    }
+
+/*
+    Exercício 2
+    Crie o metodo MediaAluno que leia primeiro nome e ultimo nome de um aluno,
+    leia 3 notas e no final exiba seu nome completo e a média das notas.
+*/
+    public void MediaAluno()
+    {
+        var primeiroNome = "José";
+        var ultimoNome = "da Silva";
+        //var nomeCompleto = primeiroNome + " " + ultimoNome;
+        var nomeCompleto = $"{primeiroNome} {ultimoNome}"; // poderia ser como a linha comentada acima, mas dessa forma é mais elegante.
+
+        double nota1 = 4.5;
+        double nota2 = 7;
+        double nota3 = 6.2;
+        double media = (nota1 + nota2 + nota3) / 3; 
+
+        Console.WriteLine($"O nome do aluno é {nomeCompleto} e sua média é {media}");
+    }
+
+/*
+    Exercício 3
+    Crie um método Frete que exiba o endereço concatenado e calcule o frete de um produto.
+    Endereço deve ter os seguintes dados: logradoura, número, cep, bairro, município e estado.
+    O Frete deve levar em conta distância, peso e volume.
+    Preço por km de distância R$1,20
+    Preço por volume R$ 5,45 por metro cúbico
+    Preço por grama R$ 0,30 
+*/
+    public void Frete()
+    {
+
+    }
+}
---
+++ 1 - Introducao ao C#/1 - variaveis-operadores/Program.cs
@@ -0,0 +1,49 @@
+﻿using tipos_operadores_exercicios;
+
+// Esse exercício visa conhecer alguns tipos e operadores existentes no C#
+// Usamos variáveis para alocar dados na memória.
+
+// Tipo string
+var minhaVariavelTipoString  = "meu texto"; // a variável foi iniciada com a string "meu texto" mas poderia ser qualquer outro texto ou até mesmo vazia.
+string minhaOutraVariavelTipoString = string.Empty; // variável iniciada como vazia.
+
+// Tipo inteiro
+var numeroInteito = 0; // variável int iniciada com 0
+int outroInteiro = 10; // iniciada com 10
+
+// Tipo double
+var meuDouble = 9.55;
+double meuOutroDouble = 0;
+
+
+// Atribuição - adicionar valor a uma variável
+minhaOutraVariavelTipoString = "Meu texto novo atribuído"; // note que tipo texto sempre é atribuido com "" aspas. 
+outroInteiro = 45;
+meuDouble = 10.99 + outroInteiro;
+
+// Concatenação - unir strings
+minhaOutraVariavelTipoString = "dotnet é " + "melhor que java"; // resultado = "dotnet é melhor que java"
+minhaVariavelTipoString = minhaVariavelTipoString + minhaOutraVariavelTipoString; // aqui concatenamos os valores dar variáveis
+
+// matemáticos
+double numeral1 = 100;
+double numeral2 = 45;
+double resultado = numeral1 - numeral2;
+Console.WriteLine("o resultado da subtração é: " + resultado);
+
+resultado = numeral1 + numeral2;
+Console.WriteLine("o resultado da soma é: " + resultado);
+
+Console.WriteLine("o resultado da divisão é: " + numeral1 / numeral2); // note que aqui faço o calculo sem atribuir a variável resultado.
+
+resultado = numeral1 * numeral2;
+Console.WriteLine("o resultado da multiplicação é " + resultado);
+
+
+var exercicios = new Exercicios();
+
+Console.WriteLine("Exercício 1");
+exercicios.ConcatenaNome();
+
+Console.WriteLine("Exercício 2");
+exercicios.MediaAluno();
